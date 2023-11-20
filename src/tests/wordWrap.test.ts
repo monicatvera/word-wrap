6edd2636653ae885cca22eca1819992f51086789
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-11-20 13:38:54 +0000

    red: makes every single line of text fit column width

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -12,5 +12,6 @@ describe('The Word Wrap', () => {
     it('makes every single line of text fit column width', () => {
       expect(wordWrap('hello', 5)).toBe('hello');
       expect(wordWrap('longword', 4)).toBe('long\nword');
+      expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
     });
 });
