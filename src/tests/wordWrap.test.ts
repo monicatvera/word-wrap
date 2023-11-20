3cf57c965853caeda6945f81e094e56427f3513c
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-11-20 13:45:03 +0000

    green: makes every single line of text fit column width

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -3,15 +3,24 @@ function wordWrap(text: string, columnWidth: number) {
         return text;
     }
     
-    const wrappedText = text.substring(0, columnWidth).concat('\n');
-    const unwrappedText = text.substring(columnWidth);
-    return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
+    let wrappedText = text.substring(0, columnWidth) + '\n';
+    let unwrappedText = text.substring(columnWidth);
+    let acumulatedText = wrappedText;
+    
+    while (unwrappedText.length > columnWidth) {
+        wrappedText = unwrappedText.substring(0, columnWidth) + '\n';
+        unwrappedText = unwrappedText.substring(columnWidth);
+        acumulatedText += wrappedText;
+    }
+    
+    return acumulatedText + unwrappedText;
 }
   
 describe('The Word Wrap', () => {
     it('makes every single line of text fit column width', () => {
       expect(wordWrap('hello', 5)).toBe('hello');
       expect(wordWrap('longword', 4)).toBe('long\nword');
+      expect(wordWrap('longword', 3)).toBe('lon\ngwo\nrd');
       expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
     });
 });
