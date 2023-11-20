e7af3de6680207d10c17421da10dafde1c4bec73
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-11-20 13:33:05 +0000

    red: makes every single line of text fit column width

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -5,5 +5,6 @@ function wordWrap(text: string, columnWidth: number) {
 describe('The Word Wrap', () => {
     it('makes every single line of text fit column width', () => {
       expect(wordWrap('hello', 5)).toBe('hello');
+      expect(wordWrap('longword', 4)).toBe('long\nword');
     });
 });
